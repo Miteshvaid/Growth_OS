bc194c70a479dc8bab6ea722612861773af4f8e3
Mitesh Vaid <miteshvaid35@gmail.com>
2026-06-28 10:41:13 +0530

    fix: update API URL to production

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+// const API_URL = "http://localhost:5000/api/auth";
 
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+});
 export const registerUser = async (userData) => {
   const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
