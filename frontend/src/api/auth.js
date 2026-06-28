a62936275410f534114b6a1a7d4e6a01a741ae65
Mitesh Vaid <miteshvaid35@gmail.com>
2026-06-28 10:53:12 +0530

    fix: auth api url

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:5000/api/auth";
-
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
+
 export const registerUser = async (userData) => {
-  const response = await axios.post(`${API_URL}/register`, userData);
+  const response = await API.post("/api/auth/register", userData);
   return response.data;
 };
 
 export const loginUser = async (userData) => {
-  const response = await axios.post(`${API_URL}/login`, userData);
+  const response = await API.post("/api/auth/login", userData);
   return response.data;
 };
